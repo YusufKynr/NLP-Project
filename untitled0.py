8fda7f5b317f3289d742c9f664da6432bf376d9e
ggekosad <141270638+YusufKynr@users.noreply.github.com>
2024-05-02 15:25:38 +0300

    Removed stopword in datas and improved split root-suffix

--- untitled0.py
+++ untitled0.py
@@ -13,30 +13,47 @@ yorumlar = pd.read_csv('veri_seti.csv')
 
 "noktalama işaretleri,sembolleri sil"
 import re 
-yorum = re.sub('[^a-zA-ZçğıöşüÇĞİÖŞÜ]',' ', yorumlar['yorum'][29])
+yorum = re.sub('[^a-zA-ZçğıöşüÇĞİÖŞÜ]',' ', yorumlar['yorum'][68])
 
-"büyük-küçük harf problemi: hepsini küçült "
+"büyük-küçük harf problemi: hepsini küçült" 
 yorum = yorum.lower()
 
 "yorumu listeye çevir"
 yorum = yorum.split()
 
 "türkçe stopwordsleri dosyadan okuyup arraye aktar"
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+stopwords = []
+with open("stopwords.txt", 'r', encoding='utf-8') as dosya:
+            for satir in dosya:
+                stopwords.append(satir.strip())  # Her satırı diziye ekle, strip() ile gereksiz boşlukları temizle
+                
+"stopwords'den arındırma"      
+def removeStopwords(array):
+    index = 0
+    while index < len(array):
+        kelime = array[index]
+        if kelime in stopwords:
+            array.pop(index)
+        else:
+            index += 1
 
-#print(stopwords)
+removeStopwords(yorum)
 
 "gövde ve eki ayrıştırma işlemi"
-
 from zeyrek import MorphAnalyzer
-
 nltk.download('punkt')
 zeyrek = MorphAnalyzer()
+
 results = []
+
 for index in range(len(yorum)):
-    result = zeyrek.lemmatize(yorum[index])
-    results.append(result[0])
+    
+    result = zeyrek.lemmatize(yorum[index].lower())
+    results.append(min(result[0][1], key=len))  # En kısa elemanı seç ve results listesine ekle
+
 print(results)
-yorum = [zeyrek.lemmatize(kelime) for kelime in yorum if not kelime in set(stopwords.words('turkish'))]
+
+
+"bazı kelimelerin birden fazla potansiyel kökü olabiliyor, bunun için bir tanesini seçiyoruz"
+
 
